d071c0ff89f715d8ebdd4a3d93d6a3c041af7930
AbdulQadeer-55 <qadeermemon100@gmail.com>
2025-01-02 08:57:09 +0000

    Solve LeetCode problem: Reverse Linked List - Commit 2

--- leetcode_solutions_2025-01-02.py
+++ leetcode_solutions_2025-01-02.py
@@ -1,1 +1,2 @@
 def twoSum(nums, target):\n    for i in range(len(nums)):\n        for j in range(i+1, len(nums)):\n            if nums[i] + nums[j] == target:\n                return [i, j]
+def reverseList(head):\n    prev = None\n    while head:\n        next_node = head.next\n        head.next = prev\n        prev = head\n        head = next_node\n    return prev
