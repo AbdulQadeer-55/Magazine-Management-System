ffaa3d5c1a4dca904da326d2945c9e4ffded75b9
AbdulQadeer-55 <qadeermemon100@gmail.com>
2025-01-02 23:48:39 +0500

    Add Contributor abstract class for managing contributor details and payment calculations

---
+++ ITMagazineManagementSystem/src/accounts/Contributor.java
@@ -0,0 +1,21 @@
+package accounts;
+
+public abstract class Contributor {
+    private final String name;
+    private final String email;
+
+    public Contributor(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract double getPaymentAmount();
+}
