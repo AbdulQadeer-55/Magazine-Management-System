189650f2295d3f9a30ee9cf7f0f4916d54c0d2bf
AbdulQadeer-55 <qadeermemon100@gmail.com>
2025-01-02 21:59:23 +0500

    Format addTwoNumbers function for improved readability

--- leetcode_solution_1_Add_Two_Numbers.py
+++ leetcode_solution_1_Add_Two_Numbers.py
@@ -1,1 +1,16 @@
-def addTwoNumbers(l1, l2):\n    carry = 0\n    result = ListNode(0)\n    current = result\n    while l1 or l2 or carry:\n        val = carry\n        if l1:\n            val += l1.val\n            l1 = l1.next\n        if l2:\n            val += l2.val\n            l2 = l2.next\n        carry, val = divmod(val, 10)\n        current.next = ListNode(val)\n        current = current.next\n    return result.next
+def addTwoNumbers(l1, l2):
+    carry = 0
+    result = ListNode(0)
+    current = result
+    while l1 or l2 or carry:
+        val = carry
+        if l1:
+            val += l1.val
+            l1 = l1.next
+        if l2:
+            val += l2.val
+            l2 = l2.next
+        carry, val = divmod(val, 10)
+        current.next = ListNode(val)
+        current = current.next
+    return result.next
